8724aba28d54e7c936d2e041650b5c297151fcc6
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-19 18:11:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-first_number = 0
-second_number = 10
+first_number = 10
+second_number = 20
 sum = first_number + second_number
 
 difference = first_number - second_number
